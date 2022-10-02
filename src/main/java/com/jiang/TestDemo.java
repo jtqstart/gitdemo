cb0b2edd3ee1099af9f5e35b0cce1e0676f84fa1
jtqstart <15660167271@163.com>
2022-10-02 09:44:49 +0800

    first commit

---
+++ src/main/java/com/jiang/TestDemo.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+/**
+ * @author jtqstart
+ * @create 2022-10-02 9:42
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("22222222222222222");
+        System.out.println("33333333333333333");
+    }
+}
